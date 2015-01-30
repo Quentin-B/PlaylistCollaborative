452d2b2ef49503fe56c2d061d70e3b024ab38a5d
Anthony Da Mota <thony-83@hotmail.fr>
2015-01-30 11:02:31 +0100

    correction bug tangible

---
+++ projet/Annees_70.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface;
+using Microsoft.Surface.Presentation;
+using Microsoft.Surface.Presentation.Controls;
+using Microsoft.Surface.Presentation.Input;
+
+namespace SurfacePaint
+{
+    /// <summary>
+    /// Interaction logic for ColorPalette.xaml
+    /// </summary>
+    public partial class Annees_70 : TagVisualization
+    {
+        public Annees_70()
+        {
+        }
+
+        private void onClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
